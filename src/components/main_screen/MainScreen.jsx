c14dce8775db0e4b36721e7a05133d678cb7ae24
Paulo Monteiro <monteiro.paulovinicius@gmail.com>
2022-06-23 16:52:47 -0400

    initial commit

--- src/components/main_screen/MainScreen.jsx
+++ src/components/main_screen/MainScreen.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom'
 
+import {CgSmileMouthOpen, CgSmileNeutral, CgSmileSad} from 'react-icons/cg'
+
 const MainScreen = () => {
 
   const navigate = useNavigate()
 
   return (
     <div className='container d-flex center-all'>
-        <div className="d-flex flex-column align-items-center card min-vw-25">
+        <div className="d-flex flex-column align-items-center card min-w-mobile">
           <h1 className='mb-4'>Test Quizz</h1>
           <div className='d-flex flex-column w-100'>
             <button className='mb-2 btn' onClick={() => navigate('quizz')}>Start Game</button>
-            <button className='mb-2 btn'>Select Dificulty</button>
+            <div className='d-flex flex-column align-items-center mb-2'>
+              <h4 className='mb-2'>Dificulty</h4>
+              <div className="d-flex justify-content-between w-75">
+                <div className="d-flex flex-column center-all">
+                  <CgSmileMouthOpen size={'5rem'}/>
+                  <h6>Easy</h6>
+                </div>
+                <div className="d-flex flex-column center-all">
+                  <CgSmileNeutral size={'5rem'}/>
+                  <h6>Medium</h6>
+                </div>
+                <div className="d-flex flex-column center-all ">
+                  <CgSmileNeutral size={'5rem'}/>
+                  <h6>Hard</h6>
+                </div>
+              </div>
+            </div>
             <button className='mb-2 btn'>See Scores</button>
           </div>
         </div>
